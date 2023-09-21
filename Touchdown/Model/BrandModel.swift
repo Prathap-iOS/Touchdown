5e0124c0aceea06f1b62e899ac510bc4801a325c
NowFloats <prathap.reddy@nowfloats.com>
2023-09-21 19:38:50 +0530

    Brand Grid View added

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Prathap Reddy on 21/09/23.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
