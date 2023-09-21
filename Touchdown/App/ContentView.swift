7fd17b93abc56bb37e20f540b98c2c9c1f0bc5df
NowFloats <prathap.reddy@nowfloats.com>
2023-09-21 18:34:01 +0530

    Scrollview addedd for player

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -22,10 +22,14 @@ struct ContentView: View {
                     .background(.white)
                     .shadow(color: .blue.opacity(0.05), radius: 5, x: 0, y: 5)
                 
-                Spacer()
-                
-                FooterView()
-                    .padding(.horizontal)
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VSTACK
+                }) //: SCROLL
             } //: VSTACK
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         } //: ZSTACK
---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Prathap Reddy on 21/09/23.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
